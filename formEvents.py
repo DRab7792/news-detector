acf90bf4d6f882df9c230e20e3da829ca5cdf7f4
Dan Rabinowitz <danr@sanbornmediafactory.com>
2017-04-19 02:23:02 -0400

    Adding more to form events.
    TODO: fix subgraph so it is the same size as the current nodes array

--- formEvents.py
+++ formEvents.py
@@ -1,10 +1,11 @@
 import mysql.connector
 import networkx as nx
-import math
+from math import *
 import inspect
 import matplotlib.pyplot as plt
-execfile("community.py")
-from scipy.cluster.hierarchy import dendrogram
+import numpy as np
+# execfile("community.py")
+from scipy.cluster.hierarchy import *
 from operator import itemgetter
 from config import getConfig
 
@@ -16,8 +17,8 @@ def getEdges(db, threshold):
 	numEdges = itemgetter(0)(numEdges)
 
 	perBatch = 1000
-	batches = int(math.ceil(numEdges/perBatch))
-	# batches = 50
+	batches = int(ceil(numEdges/perBatch))
+	batches = 5
 	
 	edges = []
 	for batch in range(batches):
@@ -79,31 +80,189 @@ def removeOrphans(graph):
 def saveGraph(graph, threshold):
 	nx.write_gml(graph, "data/article-similarities" + str(threshold) + ".gml")
 
+def formClusteringLevels(links):
+	levels = {}
+	for i in range(0, 430, 10):
+		nodeClusters = fcluster(links, float(i), 'distance')
+
+		clusterDicts = {}
+
+		for nodeId in range(len(nodeClusters)):
+			clusterId = nodeClusters[nodeId]
+			if clusterId in clusterDicts:
+				clusterDicts[clusterId].append(nodeId)
+			else:
+				clusterDicts[clusterId] = [nodeId]
+
+		levels[i] = clusterDicts
+
+	return levels
+
+# # Get all the nodes in a cluster
+# def getClusterNodes(cluster):
+# 	visited = set()
+# 	queue = [cluster]
+# 	nodes = []
+# 	# Perform breadth first search
+# 	while len(queue) > 0:
+# 		curNode = queue.pop()
+# 		if curNode and curNode not in visited:
+# 			visited.add(curNode)
+
+# 			# Create the levels map
+# 			if curNode.is_leaf():
+# 				# print ("curNode")
+# 				# print (curNode.get_left())
+# 				# print (curNode.get_right())
+# 				nodes.append(curNode.get_id())
+			
+# 			left = curNode.get_left()
+# 			right = curNode.get_right()
+			
+# 			queue.extend([left, right])
+	
+# 	return nodes
+
+def subgraph(graph, nodes):
+	newGraph = graph.copy()
+	exclude = []
+	for cur in newGraph.nodes_iter():
+		if cur not in nodes:
+			exclude.append(cur)
+	newGraph.remove_nodes_from(exclude)
+	return newGraph
+
+def calcModularities(levels, graph):
+	modularities = {}
+	graphEdges = float(graph.number_of_edges())
+	# Iterate through all levels
+	for levelDist, clusters in levels.iteritems():
+		levelModularities = []
+
+		print ("-------------------------")
+		print ("Level Distance: " + str(levelDist))
+
+		# Iterate through clusters
+		for clusterId, clusterNodes in clusters.iteritems():
+			clusterGraph = graph.subgraph(clusterNodes).copy()
+			clusterEdges = float(clusterGraph.number_of_edges())
+			clusterDegree = 0
+
+			for curNode in clusterGraph.nodes_iter():
+				clusterDegree = clusterDegree + graph.degree(curNode)
+			clusterDegree = float(clusterDegree)
+
+			adj = (clusterEdges / graphEdges)
+			rnd = pow((clusterDegree / (2 * graphEdges)), 2)
+
+
+			print ("Cluster Nodes: " + str(len(clusterNodes)))
+			# print (clusterNodes)
+			print ("Cluster Graph Nodes: " + str(len(clusterGraph)))
+			# print (clusterGraph.nodes())
+			print ("Cluster Edges: " +  str(clusterEdges))
+			print ("Real wiring: " +  str(adj))
+			print ("Cluster degree: " +  str(clusterDegree))
+			print ("Random: " + str(rnd))
+
+			clusterModularity = adj - rnd
+
+			levelModularities.append(clusterModularity)
+
+		# Calculate modularity for whole level
+		modularities[levelDist] = sum(levelModularities)
+
+	return modularities
+
+
+def saveEvents(clusters, graph, db):
+	return
+
+
+def graphModularities(modularities):
+	# Sort modularities by dist
+	xVals = sorted(modularities.keys())
+	yVals = []
+	maxI = 0
+	maxVal = 0.0
+	for i in xVals:
+		if (modularities[i] > maxVal):
+			maxVal = modularities[i]
+			maxI = i
+		yVals.append(modularities[i])
+
+	plt.plot(xVals, yVals)
+	plt.vlines(maxI, 0.0, (maxVal + 0.03), '#ff0000', 'dashed', "Optimal Modularity")
+	plt.savefig("visualizations/modularities.png")
+	plt.clf()
+
+	return maxI
+
 def cluster(graph):
-	dendrogram = generate_dendrogram(graph)
-	print (len(dendrogram))
-	for level in range(len(dendrogram)):
-		curPartition = partition_at_level(dendrogram, level)
-		print ("length of partition at level", level, "is", len(curPartition.keys()))
-	return dendrogram
+	print ("Number of nodes: " + str(len(graph)))
+	print ("Number of edges: " + str(graph.number_of_edges()))
+	matrix = nx.to_numpy_matrix(graph)
+	links = linkage(matrix, method='complete')
+	order = leaves_list(links)
+	
+	
+	tree = to_tree(links)
+	levels = formClusteringLevels(links)
+	# plotScreeplot(links)
+	modularities = calcModularities(levels, graph)
+	maxModularityDist = graphModularities(modularities)
+
+	# Visualize
+	dendrogram(
+		links,
+		truncate_mode='level',
+		p=10,
+		no_labels=True,
+		show_leaf_counts=True
+	)
+	plt.hlines(maxModularityDist, 0, 2500, '#ff0000', 'dashed', "Optimal Modularity")
+	plt.savefig("visualizations/articleLinksDendrogram.png")
+	plt.clf()
+
+def plotScreeplot(z):
+	# Plotting
+	plt.plot(range(1, len(z)+1), z[::-1, 2])
+	knee = np.diff(z[::-1, 2], 2)
+	plt.plot(range(2, len(z)), knee)
+
+	num_clust1 = knee.argmax() + 2
+	knee[knee.argmax()] = 0
+	num_clust2 = knee.argmax() + 2
+
+	plt.text(num_clust1, z[::-1, 2][num_clust1-1], 'possible\n<- knee point')
+
+	part1 = fcluster(z, num_clust1, 'maxclust')
+	part2 = fcluster(z, num_clust2, 'maxclust')
+
+	clr = ['#2200CC' ,'#D9007E' ,'#FF6600' ,'#FFCC00' ,'#ACE600' ,'#0099CC' ,
+	'#8900CC' ,'#FF0000' ,'#FF9900' ,'#FFFF00' ,'#00CC01' ,'#0055CC']
+
+	# plt.setp(title='Screeplot', xlabel='partition', ylabel='cluster distance')
+	plt.savefig('visualizations/screeplot.png')
+	plt.clf()
 
 def saveDendrogram(heirarchy):
 	graph = nx.DiGraph(heirarchy)
 	nodes = graph.nodes()
-	leaves      = set( n for n in nodes if graph.degree(n) == 0 )
+	leaves = set( n for n in nodes if graph.degree(n) == 0 )
 	inner_nodes = [ n for n in nodes if graph.degree(n) > 0 ]
 
 	# Compute the size of each subtree
 	subtree = dict( (n, [n]) for n in leaves )
 	for u in inner_nodes:
-	    children = set()
-	    node_list = list(d[u])
-	    while len(node_list) > 0:
-	        v = node_list.pop(0)
-	        children.add( v )
-	        node_list += d[v]
+		children = set()
+		node_list = list(d[u])
+		while len(node_list) > 0:
+			v = node_list.pop(0)
+			children.add( v )
+			node_list += d[v]
 
-	    subtree[u] = sorted(children & leaves)
+		subtree[u] = sorted(children & leaves)
 
 	inner_nodes.sort(key=lambda n: len(subtree[n]))
 
@@ -113,20 +272,18 @@ def saveDendrogram(heirarchy):
 	Z = []
 	k = len(leaves)
 	for i, n in enumerate(inner_nodes):
-	    children = d[n]
-	    x = children[0]
-	    for y in children[1:]:
-	        z = tuple(subtree[x] + subtree[y])
-	        i, j = index[tuple(subtree[x])], index[tuple(subtree[y])]
-	        Z.append([i, j, float(len(subtree[n])), len(z)]) # <-- float is required by the dendrogram function
-	        index[z] = k
-	        subtree[z] = list(z)
-	        x = z
-	        k += 1
+		children = d[n]
+		x = children[0]
+		for y in children[1:]:
+			z = tuple(subtree[x] + subtree[y])
+			i, j = index[tuple(subtree[x])], index[tuple(subtree[y])]
+			Z.append([i, j, float(len(subtree[n])), len(z)]) # <-- float is required by the dendrogram function
+			index[z] = k
+			subtree[z] = list(z)
+			x = z
+			k += 1
 
-	# Visualize
-	dendrogram(Z, labels=leaves)
-	plt.savefig("visualizations/articleLinksDendrogram.png");
+	
 
 def main():
 	threshold = 20
